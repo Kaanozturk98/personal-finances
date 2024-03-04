6073f6aab72c7a786225d3f5aba57f609541d201
Kaanozturk98 <kaanozturk676@gmail.com>
2024-03-04 09:33:47 +0300

    add data refresh to analytics

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -4,58 +4,66 @@ import StackedBarChart from "@component/components/StackedBarChart";
 import { AnyObject } from "@component/types";
 import { getColorForLabel } from "@component/utils";
 import { Category } from "@prisma/client";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 const Analytics = () => {
   const [datasets, setDatasets] = useState<
     { label: string; data: number[]; backgroundColor: string }[]
   >([]);
   const [labels, setLabels] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState(false); // New loading state
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`/api/analytics`);
-      const { categoryDataByMonth, categories } = await response.json();
+  // Function to fetch data
+  const fetchData = useCallback(async () => {
+    setIsLoading(true); // Start loading
+    const response = await fetch(`/api/analytics`);
+    const { categoryDataByMonth, categories } = await response.json();
 
-      const labels = Object.keys(categoryDataByMonth);
-      const datasets = categories.map((category: Category) => {
-        const data = labels.map((label) => {
-          // Find the corresponding category data for the month
-          const categoryDataForMonth = categoryDataByMonth[label].find(
-            (data: AnyObject) => data.categoryId === category.id
-          );
+    const newLabels = Object.keys(categoryDataByMonth);
+    const newDatasets = categories.map((category: Category) => {
+      const data = newLabels.map((label) => {
+        const categoryDataForMonth = categoryDataByMonth[label].find(
+          (data: AnyObject) => data.categoryId === category.id
+        );
 
-          // Return the categoryAmount, or null if not found
-          return categoryDataForMonth
-            ? categoryDataForMonth.categoryAmount
-            : null;
-        });
-
-        return {
-          label: category.name,
-          data,
-          backgroundColor: getColorForLabel(category.name),
-        };
+        return categoryDataForMonth
+          ? categoryDataForMonth.categoryAmount
+          : null;
       });
 
-      setLabels(labels);
-      setDatasets(datasets);
-    };
+      return {
+        label: category.name,
+        data,
+        backgroundColor: getColorForLabel(category.name),
+      };
+    });
 
-    fetchData();
+    setLabels(newLabels);
+    setDatasets(newDatasets);
+    setIsLoading(false); // End loading
   }, []);
+
+  // Initial data fetch
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-xl font-semibold">Monthly Expenses by Category</h1>
-      {datasets.length > 0 && labels.length > 0 ? (
+      <button onClick={fetchData} className="btn btn-base">
+        Refresh Data
+      </button>
+      {isLoading ? (
+        <span>Loading...</span>
+      ) : datasets.length > 0 && labels.length > 0 ? (
         <div className="h-[calc(100vh-216px)]">
           <Card>
             <StackedBarChart datasets={datasets} labels={labels} />
           </Card>
         </div>
       ) : (
-        <span>Loading...</span>
+        <span>No data available</span>
       )}
     </div>
   );
